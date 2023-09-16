801e870fa19bd82f12a20c235a60c81c73dfdf0d
jukha <34809502+jukha@users.noreply.github.com>
2023-09-16 12:16:07 +0500

    Learn about classical redux

--- 15-redux-intro/starter/react-app/src/store.js
+++ 15-redux-intro/starter/react-app/src/store.js
@@ -1,53 +1,13 @@
-import { createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import thunk from "redux-thunk";
+import accountReducer from "./features/account/accountSlice";
+import customerReducer from "./features/customer/customerSlice";
 
-const initialState = {
-  balance: 0,
-  loan: 0,
-  loanPurpose: "",
-};
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
 
-function reducer(state = initialState, action) {
-  switch (action.type) {
-    case "account/depost":
-      return {
-        ...state,
-        balance: state.balance + action.payload,
-      };
-    case "account/withdraw":
-      return {
-        ...state,
-        balance: state.balance - action.payload,
-      };
-    case "account/requestLoan":
-      if (state.loan > 0) return state;
-      return {
-        ...state,
-        loan: action.payload.purpose,
-      };
-    case "account/payLoan":
-      if (state.loan > 0) return state;
-      return {
-        ...state,
-        loan: 0,
-        balance: state.balance - state.loan,
-      };
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
-    default:
-      return state;
-  }
-}
-
-const store = createStore(reducer);
-
-function deposit(amount) {
-  return { type: "account/depost", payload: amount };
-}
-
-function withdraw() {}
-
-function requestLoan() {}
-
-function payloan() {}
-
-store.dispatch(deposit(600));
-console.log(store.getState());
+export default store;
